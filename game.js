2862286f4c9a750a2c9f7d4631830f35ba28f244
Tim Inman <tim.inman@gmail.com>
2015-08-03 23:01:06 -0700

    collisions

--- game.js
+++ game.js
@@ -8,28 +8,26 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {
 function preload() {
   game.stage.backgroundColor = '#BFFBFF';
   game.load.spritesheet('hb', './img/spritesheet.png', 88,72);  
-  game.load.image('lavender', './img/lavender1.png')
   game.time.advancedTiming = true;
   game.time.desiredFps = 120;
 }
+
+var flowers = [];
 function create() {
-
   game.world.setBounds(0, 0, 10000, 600);
-  s = game.add.sprite(390, 290, 'hb');
-  l = game.add.sprite(800, 640, 'lavender')
+  player = game.add.sprite(0, 290, 'hb');
 
-  l.scale.setTo(0.5, 0.5)
-  //s.animations.add('walk');
-  s.animations.add('fly', [0, 2, 4, 6,8,7,5,3,1], 60, true, 13);
-  s.animations.add('hover', [9,10,11,12,11,10], 60, true, 13);
-  s.anchor.x = 0.5;
+  //player.animationplayer.add('walk');
+  player.animations.add('fly', [0, 2, 4, 6,8,7,5,3,1], 60, true, 13);
+  player.animations.add('hover', [9,10,11,12,11,10], 60, true, 13);
+  player.anchor.x = 0.5;
 
   fuelBoard = game.add.graphics(0, 0);
 
   // set a fill and line style
   fuelBoard.beginFill(0xff005d);
   fuelBoard.lineStyle(2, 0x000000, 1);
-  
+
   // draw a shape
   fuelBoard = game.add.graphics();
   fuelBoard.lineStyle(2, 0x000000);
@@ -38,73 +36,99 @@ function create() {
   fuelBoard.fixedToCamera = true;
   fuelBoard.drawRect(50,50,200,20)
 
+  f = game.add.graphics();
+  f.lineStyle(0, 0x000000, 1)
+  f.beginFill(0x2300ff)
+  f.drawRect(50,50,200,20)
+  f.fixedToCamera = true;
 
-f = game.add.graphics();
-f.lineStyle(0, 0x000000, 1)
-f.beginFill(0x2300ff)
-f.drawRect(50,50,200,20)
-f.fixedToCamera = true;
+  //  The mask is a work-around because incremtally reducing the width of the fuel gauge moves it.
+  mask = game.add.graphics(0, 0);
+  mask.beginFill(0xffffff);
+  mask.drawRect(50, 50, 200, 20);
+  mask.fixedToCamera = true;
+  f.mask = mask;
 
-    //  A mask is a Graphics object
-    mask = game.add.graphics(0, 0);
+  
+  var lavender = game.add.bitmapData(10,10);
 
-    //  Shapes drawn to the Graphics object must be filled.
-    mask.beginFill(0xffffff);
+    lavender.ctx.beginPath();
+    lavender.ctx.rect(0,0,10,10);
+    lavender.ctx.fillStyle = '#C097E6';
+    lavender.ctx.fill();
 
-    //  Here we'll draw a circle
-    mask.drawRect(50, 50, 200, 20);
-    mask.fixedToCamera = true;
-    //  And apply it to the Sprite
-    f.mask = mask;
+    // use the bitmap data as the texture for the sprite
+    //game.add.sprite(200, 200, bmd);
 
-      for (var i = 0; i < 20; i++) {
-    game.add.image(game.world.randomX, 400, 'lavender');
+  flowers = [];
+  for (var i = 0; i < 20; i++) {
 
+    flowers.push(game.add.sprite(game.world.randomX,game.world.randomY,lavender))
   }
 
-  game.camera.follow(s); //always center player
+  game.camera.follow(player); //always center player
 
   //  This will create a new object called "cursors", inside it will contain 4 objects: up, down, left and right.
   //  These are all Phaser.Key objects, so anything you can do with a Key object you can do with these.
   cursors = game.input.keyboard.createCursorKeys();
+collisions = 0;
 
-  var text = game.add.text(32, 32, 'Arrow keys to navigate', {
-  });
 }
 
 function update() {
   // SCALE DOWN FUEL. NOT WORKING.
+  if (mask.scale.x >= 0) {
     mask.scale.x -= .001
+  }
+
     
   
   if (cursors.up.isDown) {
-    s.y += -4
+    player.y += -4
   }
   else if (cursors.down.isDown) {
-    s.y += 4;
+    player.y += 4;
   } 
  
   if (cursors.left.isDown) {
-    s.animations.play('fly');
+    player.animations.play('fly');
 
-      s.scale.x = -1
-        s.x -= 4;
+      player.scale.x = -1
+        player.x -= 4;
 
   } 
   else if (cursors.right.isDown) {
-  s.animations.play('fly');
-      s.scale.x = 1;
-    s.x += 4;
+  player.animations.play('fly');
+      player.scale.x = 1;
+    player.x += 4;
  
   } 
   else {
-    s.animations.play('hover')
+    player.animations.play('hover')
   }
+  flowers.forEach(function(flower){
+  if ((player.right >= flower.left) &&   
+      (player.right <= flower.right) && 
+      (player.top +22  >= flower.top) && 
+      (player.top + 22 <= flower.bottom)) {
+    
+    collisions +=1
+    if (mask.scale.x < 100) {
+      mask.scale += .001;
+    }
+  }
+})
+
 }
+
+// check for collision with flowers
+
+
+
 
 function render() {
 
   game.debug.cameraInfo(game.camera, 32, 500);
-     game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
+  game.debug.text('Collisions:' + (collisions), 10, 10, '00ff00')
+//  game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
 }
-body = document.getElementsByTagName('body');
